f05c146d92d62974b7815edf22e7936b1c2d8c45
petulant-annie <petulant.phantom@gmail.com>
2019-10-08 13:30:59 +0300

    add generated id from library

--- route.js
+++ route.js
@@ -1,177 +1,137 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const uuidv1 = require('uuid/v1');
 const router = express.Router();
 
-let users = require('./users');
-let articles = require('./articles');
+const users = require('./users');
+const articles = require('./articles');
 let currentUser = '';
 let currentArticle = '';
 
 function findCurrentUser(index) {
   users.data.find(user => {
-    user.id === parseInt(index) ? currentUser = user : new Error();
+    user.id.includes(index) ? currentUser = user : new Error();
   });
 }
 
 function findCurrentArticle(index) {
   articles.data.find(article => {
-    article.id === parseInt(index) ? currentArticle = article : new Error();
+    article.id.includes(index) ? currentArticle = article : new Error();
   });
 }
 
 function writeData(path, data) {
-  fs.writeFileSync(path, JSON.stringify(data));
+  fs.writeFile(path, JSON.stringify(data), (err) => { if (err) { throw err } });
 }
-
 
 router.get('/api/v1/users', (req, res, next) => {
   try {
-    console.log({ data: users.data.sort((a, b) => { b.id - a.id }) });
-    res.send({ data: users.data.sort((a, b) => { b.id - a.id }) });
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    res.send(users);
+  } catch (err) { next(err); }
 });
 
 router.get('/api/v1/users/:id', (req, res, next) => {
   try {
     findCurrentUser(req.params.id);
-
     res.send({ data: currentUser });
-  } catch (err) {
-    new Error();
-    next(err);
-  }
-
+  } catch (err) { next(err); }
 });
 
-router.post('/api/v1/users', async (req, res, next) => {
+router.post('/api/v1/users', (req, res, next) => {
   try {
-    const id = users.data.length > 0 ? users.data[users.data.length - 1].id + 1 : 0;
     const newUser = {
-      id: id,
+      id: uuidv1(),
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email
     }
-    users = { data: [...users.data, newUser] };
-    writeData('./users.json', users);
-
-    res.send(users);
+    let updUsers = { data: [newUser, ...users.data] };
+    writeData('./users.json', updUsers);
     console.log(newUser);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    res.send({ data: [newUser] });
+  } catch (err) { next(err); }
 });
 
 router.put('/api/v1/users/:id', (req, res, next) => {
   try {
-    const position = users.data.findIndex(item => item.id === parseInt(req.params.id));
+    const position = users.data.findIndex(item => item.id.includes(req.params.id));
+    console.log(position);
     users.data[position] = {
-      id: parseInt(req.params.id),
+      id: req.params.id,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email
     }
-
-    writeData('./users.json', (users));
-    res.send(users);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    writeData('./users.json', users);
+    res.send({ data: [users.data[position]] });
+  } catch (err) { next(err); }
 });
 
 router.delete('/api/v1/users/:id', async (req, res, next) => {
   try {
-    const position = users.data.findIndex(item => item.id === req.params.id);
+    const position = users.data.findIndex(item => item.id.includes(req.params.id));
     await users.data.splice(position, 1);
-
     writeData('./users.json', users);
-
     res.send(users);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+  } catch (err) { next(err); }
 });
 
 router.get('/api/v1/blog', (req, res, next) => {
   try {
-
-    res.send(articles);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    const articlesArr = articles.data.reverse();
+    res.send({ data: articlesArr });
+  } catch (err) { next(err); }
 });
 
 router.get('/api/v1/blog/:id', (req, res, next) => {
   try {
     findCurrentArticle(req.params.id);
-
     res.send({ data: currentArticle });
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+  } catch (err) { next(err); }
 });
 
 router.post('/api/v1/blog', async (req, res, next) => {
   try {
-    const id = articles.data.length > 0 ? articles.data[articles.data.length - 1].id + 1 : 0;
     const newArticle = {
-      id: id,
+      id: uuidv1(),
       title: req.body.title,
       content: req.body.content,
       author: req.body.author,
       publishedAt: req.body.publishedAt
     }
-    articles = { data: [...articles.data, newArticle] };
-    writeData('./articles.json', articles);
 
-    await res.send(articles);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    const articlesArr = { data: [...articles.data, newArticle] };
+    writeData('./articles.json', articlesArr);
+    await res.send({ data: [newArticle] });
+  } catch (err) { next(err); }
 });
 
 router.put('/api/v1/blog/:id', (req, res, next) => {
   try {
-    const position = articles.data.findIndex(item => item.id === parseInt(req.params.id));
+    const position = articles.data.findIndex(item => item.id.includes(req.params.id));
+
     articles.data[position] = {
-      id: parseInt(req.params.id),
+      id: req.params.id,
       title: req.body.title,
       content: req.body.content,
       author: req.body.author,
       publishedAt: req.body.publishedAt
     }
-
     writeData('./articles.json', articles);
-
-    res.end(articles);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    res.end({ data: [articles.data[position]] });
+  } catch (err) { next(err); }
 });
 
 router.delete('/api/v1/blog/:id', async (req, res, next) => {
   try {
-    const position = articles.data.findIndex(item => item.id === req.params.id);
+    const position = articles.data.findIndex(item => item.id.includes(req.params.id));
+
     await articles.data.splice(position, 1);
-
     writeData('./articles.json', articles);
-
-    res.send(articles);
-  } catch (err) {
-    new Error();
-    next(err);
-  }
+    const articlesArr = articles.data.reverse();
+    res.send({ data: articlesArr });
+  } catch (err) { next(err); }
 });
 
 router.get('*', (req, res) => {
