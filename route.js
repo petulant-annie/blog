ceb53a055794b34a944444a5f6d75dc59f3b0abc
petulant-annie <petulant.phantom@gmail.com>
2019-10-08 08:38:30 +0300

    pass err to error handler

--- route.js
+++ route.js
@@ -24,21 +24,30 @@ function writeData(path, data) {
   fs.writeFileSync(path, JSON.stringify(data));
 }
 
-router.get('/api/v1/users', (req, res) => {
+
+router.get('/api/v1/users', (req, res, next) => {
   try {
-    res.send(users);
-  } catch (err) { new Error(); }
+    console.log({ data: users.data.sort((a, b) => { b.id - a.id }) });
+    res.send({ data: users.data.sort((a, b) => { b.id - a.id }) });
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.get('/api/v1/users/:id', (req, res) => {
+router.get('/api/v1/users/:id', (req, res, next) => {
   try {
     findCurrentUser(req.params.id);
 
     res.send({ data: currentUser });
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
+
 });
 
-router.post('/api/v1/users', async (req, res) => {
+router.post('/api/v1/users', async (req, res, next) => {
   try {
     const id = users.data.length > 0 ? users.data[users.data.length - 1].id + 1 : 0;
     const newUser = {
@@ -47,16 +56,18 @@ router.post('/api/v1/users', async (req, res) => {
       lastName: req.body.lastName,
       email: req.body.email
     }
-
     users = { data: [...users.data, newUser] };
-
     writeData('./users.json', users);
+
     res.send(users);
     console.log(newUser);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.put('/api/v1/users/:id', (req, res) => {
+router.put('/api/v1/users/:id', (req, res, next) => {
   try {
     const position = users.data.findIndex(item => item.id === parseInt(req.params.id));
     users.data[position] = {
@@ -68,34 +79,48 @@ router.put('/api/v1/users/:id', (req, res) => {
 
     writeData('./users.json', (users));
     res.send(users);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.delete('/api/v1/users/:id', async (req, res) => {
+router.delete('/api/v1/users/:id', async (req, res, next) => {
   try {
     const position = users.data.findIndex(item => item.id === req.params.id);
     await users.data.splice(position, 1);
 
     writeData('./users.json', users);
+
     res.send(users);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.get('/api/v1/blog', (req, res) => {
+router.get('/api/v1/blog', (req, res, next) => {
   try {
+
     res.send(articles);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.get('/api/v1/blog/:id', (req, res) => {
+router.get('/api/v1/blog/:id', (req, res, next) => {
   try {
     findCurrentArticle(req.params.id);
 
     res.send({ data: currentArticle });
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.post('/api/v1/blog', async (req, res) => {
+router.post('/api/v1/blog', async (req, res, next) => {
   try {
     const id = articles.data.length > 0 ? articles.data[articles.data.length - 1].id + 1 : 0;
     const newArticle = {
@@ -105,15 +130,17 @@ router.post('/api/v1/blog', async (req, res) => {
       author: req.body.author,
       publishedAt: req.body.publishedAt
     }
-
     articles = { data: [...articles.data, newArticle] };
-
     writeData('./articles.json', articles);
+
     await res.send(articles);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.put('/api/v1/blog/:id', (req, res) => {
+router.put('/api/v1/blog/:id', (req, res, next) => {
   try {
     const position = articles.data.findIndex(item => item.id === parseInt(req.params.id));
     articles.data[position] = {
@@ -125,18 +152,26 @@ router.put('/api/v1/blog/:id', (req, res) => {
     }
 
     writeData('./articles.json', articles);
+
     res.end(articles);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
-router.delete('/api/v1/blog/:id', async (req, res) => {
+router.delete('/api/v1/blog/:id', async (req, res, next) => {
   try {
     const position = articles.data.findIndex(item => item.id === req.params.id);
     await articles.data.splice(position, 1);
 
     writeData('./articles.json', articles);
+
     res.send(articles);
-  } catch (err) { new Error(); }
+  } catch (err) {
+    new Error();
+    next(err);
+  }
 });
 
 router.get('*', (req, res) => {
