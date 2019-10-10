483b9af64d6a56ca493db1a91593c3babd5dc148
petulant-annie <petulant.phantom@gmail.com>
2019-10-10 16:42:01 +0300

    sequelize bug

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const Sequelize = require('sequelize');
 require('dotenv').config();
+const app = express();
 
 const router = require('./routes/main');
 
-const app = express();
+const PORT = process.env.PORT || 3000;
+const db = new Sequelize(
+  `${process.env.DATABASE}`,
+  `${process.env.USERNAME}`,
+  `${process.env.PASSWORD}`,
+  {
+    host: `${process.env.HOST}`,
+    dialect: 'mysql',
+  });
+
+db.authenticate()
+  .then(() => console.log('db conected'))
+  .catch((err) => console.log('Error ' + err));
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -14,4 +29,4 @@ app.use((err, req, res) => {
   res.status(500).send(err, 'Error');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3043'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
